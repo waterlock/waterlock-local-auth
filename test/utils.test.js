b509aefe4e7af51a36da1abd6cadb14447dc3d4b
Senica Gonzalez <senica@allebrum.com>
2016-10-11 07:14:17 -0700

    Add test proposed here: https://github.com/waterlock/waterlock-local-auth/pull/61#issuecomment-252507049

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,6 +4,30 @@ var waterlock = testHelper.waterlock_local;
 var proxyquire = testHelper.proxyquire;
 
 describe('utils',function(){
+
+  it('missing slash after __dirname fails', function(done) {
+    var utils = proxyquire('../lib/utils',
+      {
+        './waterlock-local-auth': waterlock,
+        'path': {
+          normalize: function(str) {
+            return __dirname+"./email.test.jade";
+          }
+        }
+      });
+
+    var error = new Error('ENOENT: no such file or directory, open \''+__dirname+'./email.test.jade\'');
+    error.errno = -2;
+    error.code = 'ENOENT';
+    error.syscall = 'open';
+    error.path = __dirname+'./email.test.jade';
+
+    (function(){
+      utils.getHtmlEmail({owner: "test", resetToken: "token"})
+    }).should.throwError(error);
+    done();
+  });
+
   var utils = proxyquire('../lib/utils',
     {
       './waterlock-local-auth': waterlock,
